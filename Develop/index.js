795198a3f7406a49d505273f723410a4806a518e
rogseo <rogseo@gmail.com>
2022-10-18 21:24:11 -0500

    init

---
+++ Develop/index.js
@@ -0,0 +1,38 @@
+// TODO: Include packages needed for this application
+const inquirer= require('inquirer');
+
+// TODO: Create an array of questions for user input
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
